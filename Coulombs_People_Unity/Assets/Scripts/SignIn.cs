5f7a6d3efc856c205611df41ab1c9f3ba79ba2ab
MineGoksen <minegoksen00@gmail.com>
2023-04-02 17:16:58 +0300

    videoPlayer Page added

--- Coulombs_People_Unity/Assets/Scripts/SignIn.cs
+++ Coulombs_People_Unity/Assets/Scripts/SignIn.cs
@@ -10,12 +10,17 @@ public class SignIn : MonoBehaviour
     public string password;
     public Button button;    
     private static Firebase.Auth.FirebaseAuth auth;
+<<<<<<< Updated upstream
 
     public Button popUp;
     public GameObject image_tip;
     public TextMeshProUGUI tip;
     string t = "eror";
 
+=======
+    public GameObject image_popup;
+    public TextMeshProUGUI popup;
+>>>>>>> Stashed changes
     void Start()
     {
         image_tip.SetActive(false);
@@ -57,19 +62,40 @@ public class SignIn : MonoBehaviour
             if (task.IsCanceled)
             {
                 //Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+<<<<<<< Updated upstream
                 tip.text="user information is incorrect";
+=======
+                popup.text = "SignInWithEmailAndPasswordAsync was canceled.";
+>>>>>>> Stashed changes
                 ShowPopUp();
                 return;
             }
             if (task.IsFaulted)
             {
-                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                //Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                popup.text = "SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception;
+                ShowPopUp();
                 return;
             }
 
             Firebase.Auth.FirebaseUser newUser = task.Result;
+            popup.text = "User signed in successfully ";
+            ShowPopUp();
             Debug.LogFormat("User signed in successfully: {0} ({1})",
                 newUser.DisplayName, newUser.UserId);
         });
+    }
+
+    public void ShowPopUp()
+    {
+        // Show the pop-up when the button is clicked
+        image_tip.SetActive(true);
+
+    }
+
+    public void HidePopUp()
+    {
+        // Hide the pop-up when the button is clicked
+        image_tip.SetActive(false);
     }
 }
